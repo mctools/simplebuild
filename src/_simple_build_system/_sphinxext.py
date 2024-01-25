0570c4102bfa4d50698bef506b5e429947b3758f
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-01-25 12:26:51 +0100

    update sphinx extension

--- src/_simple_build_system/_sphinxext.py
+++ src/_simple_build_system/_sphinxext.py
@@ -8,27 +8,54 @@ def setup(app):
     app.add_config_value('simplebuild_pkgbundles', None, 'env')
     return { 'version' : version }
 
+#FIXME: optional url-exists checking? For the CI mainly I guess.
+
 def sbpkg_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     """Link to the source code of a given simplebuild package."""
-
-    app = inliner.document.settings.env.app
-
     from .pkgutils import pkgname_valid_or_errmsg
-    pkgname = text.strip()
-    errmsg = pkgname_valid_or_errmsg( pkgname )
-    if not errmsg:
-        sbpkgs = load_sbpkgs(app)
-        errmsg = sbpkgs['error_msg']
-    if not errmsg:
-        pkginfo = sbpkgs['pkgs'].get(pkgname)
-        if not pkginfo:
-            errmsg = 'Could not find package named "%s"'%pkgname
-    if errmsg:
-        msg = inliner.reporter.error(errmsg, line=lineno)
-        return ( [inliner.problematic(rawtext, rawtext, msg)],
-                 [msg] )
-    node = _create_link_node(rawtext, app,
-                             pkgname, pkginfo['pkgdir_url'], options)
+    app = inliner.document.settings.env.app
+    def _err(msg = None):
+        _msg = inliner.reporter.error( msg or 'Syntax error in: "%s"'%text,
+                                       line = lineno)
+        return ( [inliner.problematic(rawtext, rawtext, _msg)],
+                 [_msg] )
+
+    linkinput = text.strip()
+    override_text = None
+    if '<' in linkinput:
+        _parts = [e.strip() for e in linkinput.split('<')]
+        if not len(_parts)==2 or not _parts[1].endswith('>'):
+            return _err()
+        _parts[1] = _parts[1][:-1].strip()
+        _parts = [e.strip() for e in _parts if e.strip()]
+        if len(_parts)!=2 or not all( (e and '>' not in e) for e in _parts ):
+            return _err()
+        override_text, linkinput = _parts
+
+    _parts = [p.strip() for p in linkinput.split('/',1) if p.strip()]
+    if len(_parts) not in (1,2):
+        return _err()
+    pkgname,subpath = (_parts[0],None) if len(_parts) == 1 else _parts
+    _ = pkgname_valid_or_errmsg( pkgname )
+    if _ is not None:
+        return _err(_)
+    sbpkgs = load_sbpkgs(app)
+    if sbpkgs['error_msg'] is not None:
+        return _err(sbpkgs['error_msg'])
+    pkginfo = sbpkgs['pkgs'].get(pkgname)
+    if not pkginfo:
+        return _err('Could not find package named "%s"'%pkgname)
+    url = pkginfo['pkgdir_url']
+    linktext = pkgname
+    if subpath:
+        _=pkginfo['pkgdir_local'] / subpath
+        if not _.is_file():
+            return _err(f'File not found in package: {_}')
+        url += '/%s'%subpath
+        linktext += '/%s'%subpath
+    if override_text:
+        linktext = override_text
+    node = _create_link_node(rawtext, app, linktext, url, options)
     return [node], []
 
 def _create_link_node(rawtext, app, linktext, linkurl, options):
@@ -70,11 +97,6 @@ def _actual_load_sbpkgs( app ):
                 _errfct('Duplicate package name: "%s"'%p.name)
                 break
             url = urlbase + '/' + str( p.relative_to(pkgroot) )
-            if p.name=='G4Materials':
-                print("TKTEST mapping:")
-                print("  ",p)
-                print("  ",p.relative_to(pkgroot))
-                print("  ",url)
             pkgs[ p.name ] = { 'name' : p.name,
                                'pkgdir_local' : p,
                                'pkgdir_url' : url }
