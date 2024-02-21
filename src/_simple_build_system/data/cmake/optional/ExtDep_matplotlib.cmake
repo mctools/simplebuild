5eafc4719afcc5218d4edc0e85601efdcb15ed59
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-02-21 10:47:00 +0100

    Matplotlib detection also tests pyplot import and triggers font cache build if needed

--- src/_simple_build_system/data/cmake/optional/ExtDep_matplotlib.cmake
+++ src/_simple_build_system/data/cmake/optional/ExtDep_matplotlib.cmake
@@ -1,8 +1,14 @@
 # Most packages using matplotlib does not need to specify this as an explicit
-# dependency, unless it is integral to it's functionality (a prime example would
-# be a package whose unit tests depends on numpy being available).
+# dependency, unless it is integral to it's functionality (a prime example of a
+# package needing it would be a package whose unit tests depends on matplotlib
+# being available).
 
-EXECUTE_PROCESS(COMMAND "${Python_EXECUTABLE}" "-c" "import matplotlib;print (matplotlib.__version__)"
+#NB: "import matplotlib.pyplot" (and not just "import matplotlib") is a good
+#test, but has an additional advantage in that it ensures the matplotlib font
+#cache building is triggered, so we don't get output in the middle of our unit
+#tests later..
+
+EXECUTE_PROCESS(COMMAND "${Python_EXECUTABLE}" "-c" "import matplotlib;import matplotlib.pyplot;print (matplotlib.__version__)"
                 OUTPUT_VARIABLE tmp RESULT_VARIABLE tmp_ec ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 if ("x${tmp_ec}" STREQUAL "x0")
