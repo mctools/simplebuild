252c8bc627ce2271fdab2f02672095d212c18f72
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-03-07 14:30:01 +0100

    Alternative deactivation script to fix #64

--- src/_simple_build_system/data/resources/shellrc_snippet_deactivate.sh
+++ src/_simple_build_system/data/resources/shellrc_snippet_deactivate.sh
@@ -1,9 +1,18 @@
 #This will remove any sb function defined by first redeclaring such a function
-#(in case it was somehow already removed), then unset. It will also
-#--env-unsetup any active sb project:
+#(in case it was somehow already removed), then unset:
 sb()
 {
     true
 }
 unset -f sb
-eval "$(unwrapped_simplebuild --env-unsetup)" 2>/dev/null || true
+
+#We also unsetup any active simplebuild environment. However, if invoked as a
+#conda deactivation script, CONDA_PREFIX/bin might have already been removed
+#from the path, so we reinject it in that case:
+if [ "${_SIMPLEBUILD_CURRENT_ENV:-x}" != "x" ]; then
+    if [ "${CONDA_PREFIX:-x}" != "x" -a -f "${CONDA_PREFIX:-x}/bin/sb" ]; then
+        eval "$(${CONDA_PREFIX}/bin/sb --env-unsetup 2>/dev/null)" 2>/dev/null || true
+    else
+        eval "$(sb --env-unsetup 2>/dev/null)" 2>/dev/null || true
+    fi
+fi
