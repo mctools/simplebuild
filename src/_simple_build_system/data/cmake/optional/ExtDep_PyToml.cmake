50ec948441d910ae7af7e95f62dc5cf206a3c0b4
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-12-05 09:17:46 +0100

    Add PyToml extdep which essentially tomli before Python 3.11

---
+++ src/_simple_build_system/data/cmake/optional/ExtDep_PyToml.cmake
@@ -0,0 +1,23 @@
+#This is the builtin tomllib module in python 3.11+ and otherwise it is the
+#third party tomli module.
+
+if ( "${PYTHON_VERSION_STRING}" VERSION_GREATER_EQUAL "3.11.0" )
+  set( HAS_PyToml 1 )
+  set( ExtDep_PyToml_VERSION "${PYTHON_VERSION_STRING}" )
+  set( ExtDep_PyToml_COMPILE_FLAGS "" )
+  set( ExtDep_PyToml_LINK_FLAGS "" )
+else()
+  execute_process(
+    COMMAND "${Python_EXECUTABLE}" "-c"
+    "import tomli;print (tomli.__version__)"
+    OUTPUT_VARIABLE tmp RESULT_VARIABLE tmp_ec
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if ("x${tmp_ec}" STREQUAL "x0")
+    string(STRIP "${tmp}" ExtDep_PyToml_VERSION)
+    set( ExtDep_PyToml_COMPILE_FLAGS "" )
+    set( ExtDep_PyToml_LINK_FLAGS "" )
+  else()
+    set( HAS_PyToml 0 )
+  endif()
+endif()
