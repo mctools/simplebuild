506f5298d2dbda1d0f0f952694ceda90bd77cb64
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-12-07 14:05:40 +0100

    Fix ExtDep_PyToml for python 3.10 and earlier

--- src/_simple_build_system/data/cmake/optional/ExtDep_PyToml.cmake
+++ src/_simple_build_system/data/cmake/optional/ExtDep_PyToml.cmake
@@ -1,6 +1,8 @@
 #This is the builtin tomllib module in python 3.11+ and otherwise it is the
 #third party tomli module.
 
+set( HAS_PyToml 0 )
+
 if ( "${PYTHON_VERSION_STRING}" VERSION_GREATER_EQUAL "3.11.0" )
   set( HAS_PyToml 1 )
   set( ExtDep_PyToml_VERSION "${PYTHON_VERSION_STRING}" )
@@ -14,10 +16,9 @@ else()
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
   )
   if ("x${tmp_ec}" STREQUAL "x0")
+    set( HAS_PyToml 1 )
     string(STRIP "${tmp}" ExtDep_PyToml_VERSION)
     set( ExtDep_PyToml_COMPILE_FLAGS "" )
     set( ExtDep_PyToml_LINK_FLAGS "" )
-  else()
-    set( HAS_PyToml 0 )
   endif()
 endif()
